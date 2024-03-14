bfe0044865321a3e8ccc79e0c3cd785ff1cf3e7c
PHinGJakkaphat <136901286+phingpc@users.noreply.github.com>
2024-03-14 08:02:06 +0700

    Add files via upload

---
+++ LAB 7-3-2024/Leave_app.java
@@ -0,0 +1,11 @@
+
+public class Leave_app extends EmployeeView
+{
+    public void printEmployeeDetails (String EmployeeName, String EmployeeId, String EmployeeDepartment)
+  {  
+   System.out.println("Employee Details: ");  
+   System.out.println("Name: " + EmployeeName);  
+   System.out.println("Employee ID: " + EmployeeId);  
+   System.out.println("Employee Department: " + EmployeeDepartment);  
+   } 
+}
